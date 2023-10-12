f16bd3376d53d3be4283f26d3624a38211a83eb6
Agisna Revaldo <agisrifaldo75@gmail.com>
2023-10-12 22:27:55 +0700

    create a todo list program

---
+++ todo-list/script.js
@@ -0,0 +1,28 @@
+const todos = [];
+
+const ul = document.getElementById("todos");
+
+function addTodo() {
+  const todo = document.getElementById("todo").value;
+  // const todoItem = document.getElementById("todo-item");
+
+  console.info(todo);
+
+  todos.push(todo);
+
+  // todoItem.innerText = todo;
+  
+  console.info(todos);
+
+  showTodo();
+  
+}
+
+function showTodo() {
+  for (const item of todos) {
+    const li = document.createElement("li");
+    li.innerText = item;
+
+    ul.appendChild(li);
+  }
+}
